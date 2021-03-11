2a357ac0b53a6e45d138a0da1f23d616796ed2bf
jiawei03 <jiawei03@58.com>
2021-03-11 19:09:00 +0800

    去掉无用数据源

--- robot-app/src/main/java/cn/wftank/qqrobot/app/RobotAppApplication.java
+++ robot-app/src/main/java/cn/wftank/qqrobot/app/RobotAppApplication.java
@@ -3,12 +3,13 @@ package cn.wftank.qqrobot.app;
 import cn.wftank.qqrobot.common.config.GlobalConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.io.FileNotFoundException;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 //@MapperScan("cn.wftank.qqrobot.dao.mapper")
 @EnableWebMvc
 @ComponentScan("cn.wftank.qqrobot")
