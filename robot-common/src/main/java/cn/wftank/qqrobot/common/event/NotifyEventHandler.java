a088e36a79b8385003c30bb42bdde2faea7a883e
herokillerJ <87205718@qq.com>
2021-03-11 23:12:43 +0800

    init

--- robot-common/src/main/java/cn/wftank/qqrobot/common/event/NotifyEventHandler.java
+++ robot-common/src/main/java/cn/wftank/qqrobot/common/event/NotifyEventHandler.java
@@ -1,34 +1,42 @@
 package cn.wftank.qqrobot.common.event;
 
+import cn.wftank.qqrobot.common.util.JsonUtil;
 import com.lmax.disruptor.EventHandler;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class NotifyEventHandler implements EventHandler<NotifyEventWrapper> {
 
     private static final Logger log = LoggerFactory.getLogger(NotifyEventHandler.class);
 
-    private Map<Class<? extends NotifyEvent>, EventHandler> handlerMap;
+    private Map<String, EventHandler> handlerMap;
 
     @Override
     public void onEvent(NotifyEventWrapper eventWrapper, long sequence, boolean endOfBatch) throws Exception {
-        handlerMap.get(eventWrapper.getNotifyEvent().getClass()).onEvent(eventWrapper.getNotifyEvent(),sequence,endOfBatch);
+        try {
+            Class<? extends NotifyEvent> clazz = eventWrapper.getNotifyEvent().getClass();
+            log.info(clazz.getName()+"event receive,detail"+ JsonUtil.toJson(eventWrapper));
+            handlerMap.get(clazz.getName()).onEvent(eventWrapper.getNotifyEvent(),sequence,endOfBatch);
+        }catch (Exception e){
+            log.error("on event ex:"+ ExceptionUtils.getStackTrace(e));
+            throw new RuntimeException(e);
+        }
     }
 
     public static class Builder{
-        private Map<Class<? extends NotifyEvent>, EventHandler> handlerMap = new HashMap<>();
+        private Map<String, EventHandler> handlerMap = new ConcurrentHashMap<>();
 
         public void addEventHandler(Class<? extends NotifyEvent> clazz, EventHandler eventHandler){
-            handlerMap.put(clazz,eventHandler);
+            handlerMap.put(clazz.getName(),eventHandler);
         }
 
         public NotifyEventHandler build(){
             NotifyEventHandler notifyEventHandler = new NotifyEventHandler();
-            notifyEventHandler.handlerMap = Collections.unmodifiableMap(this.handlerMap);
+            notifyEventHandler.handlerMap = this.handlerMap;
             return notifyEventHandler;
         }
     }
