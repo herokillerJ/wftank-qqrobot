58ddf594b377dc353013e43cbf4054a5d5b55197
herokillerJ <87205718@qq.com>
2021-03-21 23:22:26 +0800

    优化消息发送代码

--- robot-app/src/main/java/cn/wftank/qqrobot/app/handler/DiscordEventHandlers.java
+++ robot-app/src/main/java/cn/wftank/qqrobot/app/handler/DiscordEventHandlers.java
@@ -10,7 +10,6 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.Embed;
 import discord4j.core.object.entity.Attachment;
 import discord4j.core.object.entity.Message;
-import net.mamoe.mirai.message.data.MessageChainBuilder;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,9 +19,8 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -40,54 +38,53 @@ public class DiscordEventHandlers {
             Message message = event.getMessage();
             log.info("discord receive message:"+message.getContent());
             if (!filterMessage(message)) return Mono.empty();
-            MessageChainBuilder builder = new MessageChainBuilder();
-            builder.add("Discord消息：");
+            List msgList = new LinkedList<>();
+            msgList.add("Discord消息：");
             //发言人
             message.getAuthor().ifPresent(author -> {
-                builder.add("\n"+author.getTag()+"说：");
+                msgList.add("\n"+author.getTag()+"说：");
             });
             //消息体
             if (StringUtils.hasText(message.getContent())){
-                builder.add("\n"+message.getContent());
+                msgList.add("\n"+message.getContent());
                 //翻译
-                builder.add("[机翻:"+translator.translateEn2Cn(message.getContent())+"]");
+                msgList.add("[机翻:"+translator.translateEn2Cn(message.getContent())+"]");
             }
-            List<File> images = new ArrayList<>();
             //附件 一般是图片
             Set<Attachment> attachments = message.getAttachments();
             if (!CollectionUtils.isEmpty(attachments)){
                 for (Attachment attachment: attachments) {
-                    images.add(FileUtil.downloadAsTmpFromUrl(attachment.getUrl()));
+                    msgList.add(FileUtil.downloadAsTmpFromUrl(attachment.getUrl()));
                 }
             }
             //内嵌消息
             List<Embed> embeds = event.getMessage().getEmbeds();
             if (!CollectionUtils.isEmpty(embeds)){
-                builder.add("\n内嵌消息：");
+                msgList.add("\n内嵌消息：");
                 embeds.stream().forEach(embed -> {
-                    embed.getAuthor().ifPresent(author -> builder.add("\n作者："+author.getName()+""));
+                    embed.getAuthor().ifPresent(author -> msgList.add("\n作者："+author.getName()+""));
                     embed.getTitle().ifPresent(title -> {
-                        builder.add("\n标题："+title+"");
-                        builder.add("[机翻："+translator.translateEn2Cn(title)+"]");
+                        msgList.add("\n标题："+title+"");
+                        msgList.add("[机翻："+translator.translateEn2Cn(title)+"]");
                     });
                     embed.getDescription().filter(StringUtils::hasText)
                             .ifPresent(des -> {
-                                builder.add("\n描述："+des);
-                                builder.add("[机翻："+translator.translateEn2Cn(des)+"]");
+                                msgList.add("\n描述："+des);
+                                msgList.add("[机翻："+translator.translateEn2Cn(des)+"]");
                             });
                     embed.getImage().ifPresent(image -> {
                         try {
-                            builder.add("\n图片："+image.getUrl());
-                            images.add(FileUtil.downloadAsTmpFromUrl(image.getUrl()));
+                            msgList.add("\n图片："+image.getUrl());
+                            msgList.add(FileUtil.downloadAsTmpFromUrl(image.getUrl()));
                         } catch (Exception e) {
                             log.error(ExceptionUtils.getStackTrace(e));
                         }
                     });
-                    embed.getVideo().ifPresent(video -> builder.add("\n视频："+video.getUrl()));
-                    embed.getUrl().ifPresent(url -> builder.add("\n链接："+url));
+                    embed.getVideo().ifPresent(video -> msgList.add("\n视频："+video.getUrl()));
+                    embed.getUrl().ifPresent(url -> msgList.add("\n链接："+url));
                 });
             }
-            sender.sendMessageForAllGroups(builder.build(),images);
+            sender.sendMessageForAllGroups(msgList);
         }catch (Exception e){
             log.error(ExceptionUtils.getStackTrace(e));
         }
--- robot-app/src/main/java/cn/wftank/qqrobot/app/mirai/QQGroupMessageSender.java
+++ robot-app/src/main/java/cn/wftank/qqrobot/app/mirai/QQGroupMessageSender.java
@@ -16,10 +16,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
@@ -58,6 +55,40 @@ public class QQGroupMessageSender {
 
     public void sendMessageForAllGroups(MessageChain messageChain){
         sendMessageForAllGroups(messageChain, null);
+    }
+
+    public void sendMessageForAllGroups(List messageList){
+        if (!bot.isOnline()){
+            log.warn("bot is offline,will login again");
+            bot.login();
+        }
+        //获取配置的QQ群
+        String groupsStr = GlobalConfig.getConfig(ConfigKeyEnum.GROUPS);
+        //使用有序set
+        Set<Long> collect = Arrays.stream(groupsStr.split(",")).map(Long::valueOf).collect(Collectors.toCollection(LinkedHashSet::new));
+        collect.forEach(groupId -> {
+            //每个群延时发送防止被当做机器人
+            try {
+                MessageChainBuilder messageBuilder = new MessageChainBuilder();
+                Group group = bot.getGroup(groupId);
+                if (CollectionUtils.isNotEmpty(messageList)){
+                    ListIterator iterator = messageList.listIterator();
+                    while (iterator.hasNext()){
+                        Object message = iterator.next();
+                        if (message instanceof String){
+                            messageBuilder.add((String)message);
+                        }else if (message instanceof File){
+                            File file  = (File) message;
+                            messageBuilder.add(group.uploadImage(ExternalResource.create(file)));
+                        }
+                    }
+                }
+                group.sendMessage(messageBuilder.build());
+                Thread.sleep(Duration.ofSeconds(5).toMillis());
+            } catch (Exception e) {
+                log.error("send message to group:{} exception:{}",groupId,ExceptionUtils.getStackTrace(e));
+            }
+        });
     }
 
 }
