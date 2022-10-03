7c0499f5fd056a19dd3589cfcb82388c09617240
herokillerJ <87205718@qq.com>
2022-10-03 18:59:30 +0800

    解决循环依赖

--- robot-app/src/main/java/cn/wftank/qqrobot/app/event/handler/CommandEventHandler.java
+++ robot-app/src/main/java/cn/wftank/qqrobot/app/event/handler/CommandEventHandler.java
@@ -15,6 +15,7 @@ import net.mamoe.mirai.message.data.MessageUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -29,6 +30,7 @@ public class CommandEventHandler implements EventHandler<CommandNotifyEvent> {
     private QQGroupMessageSender qqGroupSender;
 
     @Autowired
+    @Lazy
     private CommonCommandHandler commonCommandHandler;
 
     @Override
